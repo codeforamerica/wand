d7745db2ef1c33ba62f16bd9e10048c4c1920d8a
mapsam <matthews.sam@gmail.com>
2015-07-15 15:49:01 -0700

    refactor to augmented module pattern, fixes #16, adding plugin example

--- src/js/wand.js
+++ src/js/wand.js
@@ -1,51 +1,60 @@
-(function() {
-  Wand = function(options) {
-    // @params options - object that contains nodes and html container id
-    this.options = options || {} ;// {} if no options, empty object
-    if (!options.elem || !document.getElementById(options.elem)) {
-      throw badHtmlError;
-    }
+var Wand = (function() {
 
-    this.nodes = options.nodes;
-    this.elem = document.getElementById(options.elem);
-    this.elem.className += ' wand';
+  var wand = {};
 
-    renderNode(0);
-  };
+  var options = {},
+      nodes = [],
+      elem = null;
 
   var badHtmlError = new Error('You did not pass a valid HTML Element');
 
+  wand.init = function(opts) {
+    // @params options - object that contains nodes and html container id
+    options = opts || {} ;// {} if no options, empty object
+    if (!opts.elem || !document.getElementById(opts.elem)) {
+      throw badHtmlError;
+    }
+
+    nodes = opts.nodes;
+    elem = document.getElementById(opts.elem);
+    elem.className += ' wand';
+
+    wand.renderNode(0);
+  };
+
   function getNode(nodeId) {
-    for (var i = this.nodes.length - 1; i >= 0; i--) {
-      if (this.nodes[i].id === nodeId) {
-        return this.nodes[i];
+    for (var i = nodes.length - 1; i >= 0; i--) {
+      if (nodes[i].id === nodeId) {
+        return nodes[i];
       }
     }
     console.error('Node does not exist:', nodeId);
     return;
   }
 
-  function renderNode(nodeId) {
+  wand.renderNode = function(nodeId) {
     var node = getNode(nodeId);
     if (!node) { throw new Error('redundant unfound node error'); }
     // puts stuff into a template
-    this.elem.innerHTML = '<h1>' + node.title + '</h1>';
-    this.elem.innerHTML += '<div class="node-contents">' + node.content + '</div>';
+    elem.innerHTML = '<h1>' + node.title + '</h1>';
+    elem.innerHTML += '<div class="node-contents">' + node.content + '</div>';
     if (node.triggers) {
       for (var i = node.triggers.length - 1; i >= 0; i--) {
         renderTrigger(node.triggers[i]);
       }
     }
-  }
+  };
 
   function renderTrigger(trigger) {
     var button = document.createElement('button');
     button.innerHTML = trigger.content;
     button.onclick = function (event) {
-      renderNode(trigger.target);
+      wand.renderNode(trigger.target);
     };
 
-    this.elem.appendChild(button);
+    elem.appendChild(button);
   }
+
+  return wand;
 
 }());
